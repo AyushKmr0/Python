ebdfc5d334e291dae7e906b1d28d58e265062efc
Ayush Kumar <yeashr007@gmail.com>
2025-12-27 20:36:51 +0530

    handling apis

---
+++ 10_handling_apis/freeapi_username.py
@@ -0,0 +1,56 @@
+# import requests
+
+# def fetch_random_user():
+#     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+#     response = requests.get(url)
+#     data = response.json()
+    
+#     if data['success'] and 'data' in data:
+#         user_data = data["data"]
+#         username = user_data["login"]["username"]
+#         location = user_data["location"]["country"]
+        
+#         return username, location
+#     else:
+#         raise Exception("Failed to fetch user data.")
+
+# def main():
+#     try:
+#         username, location = fetch_random_user()
+#         print(f"Username: {username} \nLocation: {location}")
+    
+#     except Exception as e:
+#         print(str(e))
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+import requests
+
+def fetch_random_username():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
+
+    if data['success'] and 'data' in data:
+        user_data = data['data']
+        username = user_data['login']['username']
+        country = user_data['location']['country']
+        
+        return username, country
+    else:
+        raise Exception("Failed to fetch user data.")
+    
+
+def main():
+    try:
+        username, country = fetch_random_username()
+        print(f"Username: {username} \nCountry: {country}")
+    
+    except Exception as e:
+        print(str(e))
+
+if __name__ == '__main__':
+    main()
